e1839a08de64a46ee25a0da61d4a02668766a279
Alexia90 <alexia.ostermann@gmail.com>
2019-06-20 10:29:57 +0200

    third_commit

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "string.h"
 
-void initContener(int nb_conteneurs, int largeur, int hauteur) {
+void initContainerr() {
     FILE* fp = NULL;
     fp = fopen("instances/1_global.csv", "r");
 
@@ -12,11 +12,32 @@ void initContener(int nb_conteneurs, int largeur, int hauteur) {
     }
     do {
         fscanf(fp, "%d , %d , %d", nb_conteneurs, largeur, hauteur);
-    } 
+    }
 
     fclose(fp);
     return 1;
   }
 
+void placerContainer(Port *P) {
+  int caractereLu = 0;
+  int ligne = 0;
+  FILE *fp = NULL;
+  fp = fopen("instances/1_position.csv", "r");
 
+  if (fp == NULL) {
+      printf("\nImpossible de charger l'instance");
+      return 0;
+  }
 
+  while (caractereLu != EOF){
+    if (ligne>0)
+      {
+        Container *C = (Container *)malloc(sizeof(Container));
+        fscanf(fp, "%s, %d, %d", C->name, C->posX, C->posY);
+        C->id = ligne;
+        P->baie[PosX-1][PosY-1]=C;
+      }
+    ligne++;
+  }
+
+}
